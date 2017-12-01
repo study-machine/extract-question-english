b4c543582f9579082ff518bf4b78e392c5603375
study-machine <471236183@qq.com>
2017-12-01 22:34:13 +0800

    dev

--- tiku_orm/config.py
+++ tiku_orm/config.py
@@ -2,5 +2,5 @@
 
 from db import *
 
-db_config_read = DatabaseConfig.local_db
-db_config_write = DatabaseConfig.local_db
+db_config_read = DatabaseConfig.dev_db
+db_config_write = DatabaseConfig.dev_db
