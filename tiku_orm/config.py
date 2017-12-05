18b8e5828c55b1db369879a94f619a77dd14ee2f
study-machine <471236183@qq.com>
2017-12-05 16:10:17 +0800

    .

--- tiku_orm/config.py
+++ tiku_orm/config.py
@@ -1,6 +1,6 @@
-# coding=utf-8
+# coding=utf8
 
 from db import *
 
 db_config_read = DatabaseConfig.online_slave_db
-db_config_write = DatabaseConfig.dev_db
+db_config_write = DatabaseConfig.local_db
