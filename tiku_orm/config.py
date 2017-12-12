4cc0196ef472f174c1982bd5f8bb92b9e4b18218
study-machine <471236183@qq.com>
2017-12-12 21:47:43 +0800

    stater bug re online

--- tiku_orm/config.py
+++ tiku_orm/config.py
@@ -3,7 +3,7 @@
 from db import *
 
 db_config_read = DatabaseConfig.online_slave_db
-db_config_write = DatabaseConfig.dev_knowboxstore_db
+db_config_write = DatabaseConfig.online_knowboxstore_db
 
 # write_type = 'tiku'
 write_type = 'store'
